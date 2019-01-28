35ee45e5ce9fa6333ccb0cbdf78cf92f87212f18
jerrodthomas <38600082+jerrodthomas@users.noreply.github.com>
2019-01-27 23:55:54 -0600

    Add files via upload

---
+++ scripts/clock.js
@@ -0,0 +1,5 @@
+var myVar = setInterval(myTimer, 1000);
+            function myTimer() {
+                var d = new Date();
+                document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+            }
