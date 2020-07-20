1d1f00f882e8fb2581f19ff207c4d4ba59b96fdb
jerrodthomas <38600082+jerrodthomas@users.noreply.github.com>
2020-07-20 12:35:07 -0500

    Delete clock.js

--- scripts/clock.js
+++
@@ -1,5 +0,0 @@
-var myVar = setInterval(myTimer, 1000);
-            function myTimer() {
-                var d = new Date();
-                document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-            }
