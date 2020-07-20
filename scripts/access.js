24780dd96773565961872f7828b465ff3096d610
jerrodthomas <38600082+jerrodthomas@users.noreply.github.com>
2020-07-20 12:34:32 -0500

    Delete access.js

--- scripts/access.js
+++
@@ -1,9 +0,0 @@
-/* exported myFunction */
-"use strict";
-
-function myFunction() {
-    var person = prompt("Please enter your name", " ");
-    if (person != null) {
-        document.getElementById("demo").innerHTML = "ACCESS GRANTED " + person + ", WELCOME.";
-    }
-}
